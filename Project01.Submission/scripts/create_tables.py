e9810b027c05755ef5db578a563ffdd216121875
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-08-12 21:20:57 -0700

    submited files

--- Project01.Submission/scripts/create_tables.py
+++ Project01.Submission/scripts/create_tables.py
@@ -3,19 +3,22 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def create_database():
-    # connect to default database
+    """ Create a connection to postgre DB
+        Set the session property with auto commit= true to commit  after every execution
+        return variables for connection and cursor
+        
+        
+    """
     conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
     
-    # create sparkify database with UTF8 encoding
+    
     cur.execute("DROP DATABASE IF EXISTS sparkifydb")
     cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
 
-    # close connection to default database
     conn.close()    
-    
-    # connect to sparkify database
+        
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
     
@@ -23,12 +26,14 @@ def create_database():
 
 
 def drop_tables(cur, conn):
+    """for all the queries in the List drop_table_queries execute each statement and commit"""
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """for all the queries in the List create_table_queries execute each statement and commit"""
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
@@ -36,10 +41,8 @@ def create_tables(cur, conn):
 
 def main():
     cur, conn = create_database()
-    
     drop_tables(cur, conn)
     create_tables(cur, conn)
-
     conn.close()
 
 
--- Project01.Submission/scripts/etl.py
+++ Project01.Submission/scripts/etl.py
@@ -10,11 +10,11 @@ def process_song_file(cur, filepath):
     df = pd.read_json(filepath,lines=True)
 
     # insert song record
-    song_data = list(df[['song_id','title','artist_id', 'year','duration']].values[0])
+    song_data = list(df.loc[:,['song_id','title','artist_id', 'year','duration']].values[0])
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data =  list(df[['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']].values[0])
+    artist_data = list(df.loc[:,['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']].values[0])
     cur.execute(artist_table_insert, artist_data)
 
 
@@ -39,7 +39,7 @@ def process_log_file(cur, filepath):
                        ,'month':pd.to_datetime(df.ts,unit='ms').dt.month
                        ,'year':pd.to_datetime(df.ts,unit='ms').dt.year
                         ,'weekday':pd.to_datetime(df.ts,unit='ms').dt.weekday
-                       })
+                       }) 
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
